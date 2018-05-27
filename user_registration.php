8db2d2acd87f5928a2d46f3184d8f62505c9b975
shoukhin93 <shoukhin1993@gmail.com>
2018-05-27 22:50:17 +0600

    user registration api updated. JSON format included

--- user_registration.php
+++ user_registration.php
@@ -1,23 +1,45 @@
 <?php
 include('connection.php');
 
+$response = array();
+
 if (!isset($_POST['name']) || !isset($_POST['password']) || !isset($_POST['email']) ||
     !isset($_POST['phone'])
 ) {
-    echo "Please fill all fields correctly!";
+    $response['error'] = true;
+    $response['message'] = 'Please fill all fields correctly';
 } else {
     $name = $_POST['name'];
     $password = $_POST['password'];
     $email = $_POST['email'];
     $phone = $_POST['phone'];
 
-    $user_registration_query = "insert into users (name,password,email,phone) 
+    $user_exist_query = "SELECT * FROM users WHERE email = '$email' OR phone = '$phone'";
+    $user_exist = $connection->query($user_exist_query);
+
+    if ( mysqli_num_rows($user_exist) > 0) {
+        $response['error'] = true;
+        $response['message'] = 'Email or phone already used!';
+    } else {
+
+        $user_registration_query = "insert into users (name,password,email,phone) 
                               VALUES ('$name','$password','$email','$phone')";
 
-    $isInserted = $connection->query($user_registration_query);
+        $connection->query($user_registration_query);
 
-    if ($isInserted) {
-        echo "Successfully registered!";
-    } else
-        echo "Something went wrong! Please try again";
+        $user = array(
+            'name' => $username,
+            'email' => $email,
+            'phone' => $phone
+        );
+
+        $response['error'] = false;
+        $response['message'] = "Successfully registered!";
+        $response['user'] = $user;
+
+    }
+
+
 }
+
+echo json_encode($response);
