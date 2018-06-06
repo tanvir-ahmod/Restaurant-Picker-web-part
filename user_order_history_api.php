1f99f3480efdceb4297f6d9ee894fb6a25aebc14
shoukhin93 <shoukhin1993@gmail.com>
2018-06-06 16:31:23 +0600

    user order history api changed. item name and restaurant name added

--- user_order_history_api.php
+++ user_order_history_api.php
@@ -20,14 +20,23 @@ if (isset($_GET['id'])) {
     $response['history'] = array();
 
     while ($row = $result->fetch_assoc()) {
-
         $item_id = $row['item_id'];
+        $restaurant_id = $row['company_id'];
         $amount = $row['amount'];
 
         $get_price_query = "SELECT price FROM item WHERE id = '$item_id'";
-        $price = $connection->query($get_price_query) ->fetch_assoc();
+        $price = $connection->query($get_price_query)->fetch_assoc();
         $price['price'] = $price['price'] * $amount;
         $row['price'] = $price['price'];
+
+        $get_restaurant_name_query = "SELECT name FROM company WHERE id = '$restaurant_id'";
+        $restaurant_name = $connection->query($get_restaurant_name_query)->fetch_assoc();
+        $row['restaurant_name'] = $restaurant_name['name'];
+
+        $get_item_name_query = "SELECT item_name FROM item WHERE id = '$item_id'";
+        $item_name = $connection->query($get_item_name_query)->fetch_assoc();
+        $row['item_name'] = $item_name['item_name'];
+
         array_push($response['history'], $row);
 
     }
